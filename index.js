08727285511b2804b8e58a6384733daa59edb33f
ess <panuwat@digitopolisstudio.com>
2020-08-03 14:44:03 +0700

    fix: wrong type payload

--- index.js
+++ index.js
@@ -56,10 +56,10 @@ app.post('/api/line/lottery', (req, res) => {
 
       const todaysDate = new Date()
       const currentYear = todaysDate.getFullYear()
-      let resp = lotto.getPeriodsByYear({"year":currentYear,"type":"CHECKED"})
+      let resp = lotto.getPeriodsByYear({"year":currentYear.toString(),"type":"CHECKED"})
       console.log("currentYear " + currentYear +  "resp " + resp.response + " typeof " + typeof currentYear)
       if(resp && resp.response){
-        let respPrize =  lotto.checkLotteryResult({"number":[{"lottery_num":currentYear.toString()}],"period_date":resp.response.result[0].date})
+        let respPrize =  lotto.checkLotteryResult({"number":[{"lottery_num":text}],"period_date":resp.response.result[0].date})
         if(respPrize && respPrize.response){
                 console.log("respPrize " + respPrize)
                 if(respPrize.response.result[0].statusType === 1) {
